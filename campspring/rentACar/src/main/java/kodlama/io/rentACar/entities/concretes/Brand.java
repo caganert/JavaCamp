910c6205ea72fc4c83d4df2952793252c2f546b4
Çağan Ege Ertuğrul <caganegeertugrul@gmail.com>
2022-11-16 19:19:33 +0300

    .

--- campspring/rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
+++ campspring/rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -1,30 +1,23 @@
 package kodlama.io.rentACar.entities.concretes;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Table(name="brands")
+@Data
+//@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//Id otomatik 1 arttırır.
+    @Column(name="id")
     private int id;
+
+    @Column(name="name")
     private String name;
-
-    public Brand() {
-    }
-
-    public Brand(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
