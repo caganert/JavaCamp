f1f2593d166947a4f8df8eec14ea6cdd250f00df
Çağan Ege Ertuğrul <caganegeertugrul@gmail.com>
2023-02-23 20:47:50 +0300

    .

--- campspring/rentACar/src/main/java/kodlama/io/rentACar/business/abstracts/BrandService.java
+++ campspring/rentACar/src/main/java/kodlama/io/rentACar/business/abstracts/BrandService.java
@@ -1,12 +1,17 @@
 package kodlama.io.rentACar.business.abstracts;
 
 import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.requests.UpdateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.business.responses.GetByIdBrandResponse;
 import kodlama.io.rentACar.entities.concretes.Brand;
 
 import java.util.List;
 
 public interface BrandService {
     List<GetAllBrandsResponse> getAll();
+    GetByIdBrandResponse getById (int id);
     void add(CreateBrandRequest createBrandRequest);
+    void updateBrandRequest(UpdateBrandRequest updateBrandRequest);
+    void delete(int id);
 }
--- campspring/rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
+++ campspring/rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -2,32 +2,45 @@ package kodlama.io.rentACar.webApi.controllers;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
 import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.requests.UpdateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.business.responses.GetByIdBrandResponse;
 import kodlama.io.rentACar.entities.concretes.Brand;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/brands")
+@AllArgsConstructor
 public class BrandsController {
     private BrandService brandService;
-
-    @Autowired
-    public BrandsController(BrandService brandService) {
-        this.brandService = brandService;
-    }
-    @GetMapping("/getAll")
+    @GetMapping()
     public List<GetAllBrandsResponse> getAll(){
         return brandService.getAll();
     }
 
-    @PostMapping("/add")
+    @GetMapping("/{id}")
+    public GetByIdBrandResponse getById(@PathVariable int id){
+        return brandService.getById(id);
+    }
+
+    @PostMapping()
+    @ResponseStatus(code= HttpStatus.CREATED)
     public void add(CreateBrandRequest createBrandRequest){
         this.brandService.add(createBrandRequest);
+    }
+
+    @PutMapping
+    public void update(@RequestBody UpdateBrandRequest updateBrandRequest){
+        this.brandService.updateBrandRequest(updateBrandRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+        this.brandService.delete(id);
     }
 }
